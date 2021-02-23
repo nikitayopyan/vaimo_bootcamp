bc0f52c08b6ddb5a23b3d7867095860947e2f9a7
mykyta.yanushevskyi mykyta.yanushevskyi@vaimo.com <mykyta.yanushevskyi@vaimo.com>
2021-02-23 10:50:26 +0200

    added generate button+updated script

--- app/code/Nikita/Promotion/view/frontend/web/js/script.js
+++ app/code/Nikita/Promotion/view/frontend/web/js/script.js
@@ -1,21 +1,33 @@
 define(function () {
 
-    var submitFormBtn = document.querySelector('#formSubmit');
+    var generateFormBtn = document.querySelector('#formGenerate');
+    var copyFormBtn = document.querySelector('#formCopy');
     var formInput = document.querySelector('.promotion__input input');
 
-    function copyToClipboard(input) {
-        input.select()
+
+    if (localStorage.getItem('link')) {
+        formInput.value = localStorage.getItem('link')
+        generateFormBtn.style.display = 'none'
+        copyFormBtn.style.display = 'block'
+    }
+
+    function copyUniqueUrl() {
+        formInput.select()
         document.execCommand('copy')
     }
 
-    submitFormBtn.addEventListener('click', function (event) {
-        event.preventDefault();
+    function generateUrl() {
 
         let randomUrl = Math.random().toString(18).substr(1, 10)
         let reg = randomUrl.match('[a-zA-Z0-9]*$')
         formInput.value = `http://neekeetah.test/${reg.join('')}`;
 
-        copyToClipboard(formInput);
-    }, {once: true})
+        localStorage.setItem('link', formInput.value)
 
+        generateFormBtn.style.display = 'none'
+        copyFormBtn.style.display = 'block'
+    }
+
+    copyFormBtn.addEventListener('click', copyUniqueUrl)
+    generateFormBtn.addEventListener('click', generateUrl, {once: true})
 })
